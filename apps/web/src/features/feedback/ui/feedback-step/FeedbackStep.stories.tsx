9d980758272d9ea5400d3c335bfb7c1cf736f420
JeongwooHam <hamjw0122@gmail.com>
2024-11-09 17:59:10 +0900

    [ANI-44-progress-step] chore: 파일 위치 변경에 따른 import 경로 수정

--- apps/web/src/features/feedback/ui/feedback-step/FeedbackStep.stories.tsx
+++ apps/web/src/features/feedback/ui/feedback-step/FeedbackStep.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import FeedbackStep from '.'
-import { FEEDBACK_STEP } from '@/types/feedback/feedback-step'
+import { FEEDBACK_STEP } from '../../model/feedback-step'
 
 const meta = {
   title: 'Widget/Feedback/FeedbackStep',
