14f2898eabf27c5c1b37c2d3dcc7bc7473eeaaf4
zivivle <123868471+zivivle@users.noreply.github.com>
2024-11-10 18:50:06 +0900

    [ANI-58-pass-product-item] fix:PprogressBar transformOrigin: "center left"로 변경

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { PassProductItemBasic } from '@repo/ui/pass-product-item'
 import Typography from '@repo/ui/typography'
 
 export default function Home() {
@@ -12,23 +11,6 @@ export default function Home() {
         color='disable'>
         Hello
       </Typography>
-      {/* TODO: ImageSection random image -> placeholder 이미지로 변경 */}
-      {/* TODO: progressbar transformOrigin: "center left"로 스타일 수정*/}
-      <PassProductItemBasic>
-        <PassProductItemBasic.Image
-          src='/avif/placeholder.avif'
-          alt='테스트이미지'
-        />
-        <PassProductItemBasic.BadgeList
-          level={1}
-          category='카테고리'
-          badgeType='IN_PROGRESS'
-        />
-        <PassProductItemBasic.TextContentList
-          name='패스 이름'
-          period='2024.09.06 ~ 2024.10.06'
-        />
-      </PassProductItemBasic>
     </div>
   )
 }
