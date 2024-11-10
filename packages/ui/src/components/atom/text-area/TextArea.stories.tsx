8251a8476b7ffd5fa4b8b6d43bbdf96fbc8c2513
JeongwooHam <hamjw0122@gmail.com>
2024-11-10 21:50:53 +0900

    [ANI-46-text-area] feat: Indicator 유무에 따른 story 분리

--- packages/ui/src/components/atom/text-area/TextArea.stories.tsx
+++ packages/ui/src/components/atom/text-area/TextArea.stories.tsx
@@ -27,6 +27,24 @@ export const Primary: Story = {
           className='h-[12.5rem] w-[31.5rem] p-4'
           maxLength={500}
         />
+      </TextArea>
+    </div>
+  )
+}
+
+export const WithIndicator: Story = {
+  args: {
+    defaultValue: ''
+  },
+
+  render: (args) => (
+    <div>
+      <TextArea {...args}>
+        <TextArea.form
+          placeholder='취소 사유를 입력해주세요'
+          className='h-[12.5rem] w-[31.5rem] p-4'
+          maxLength={500}
+        />
         <TextArea.indicator maxLength={500} />
       </TextArea>
     </div>
