6f6524d18ccc2eca5d6cf9418172f312452babb2
Zero1016 <iolpd42@gmail.com>
2024-11-10 17:03:14 +0900

    [ANI-59-storybook] refactor: globals.css를 하나로 통일합니다.

--- apps/web/.storybook/preview.tsx
+++ apps/web/.storybook/preview.tsx
@@ -1,5 +1,5 @@
 import type { Preview } from '@storybook/react'
-import '@/styles/globals.css'
+import '../../../config/libs/globals.css'
 import { pretendardFont } from '../app/_fonts'
 import React from 'react'
 
