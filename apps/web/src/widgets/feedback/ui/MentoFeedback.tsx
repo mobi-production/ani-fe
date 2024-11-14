882868e3ceb4452a3eaff41b7e53f16dc7a92062
JeongwooHam <hamjw0122@gmail.com>
2024-11-15 00:52:31 +0900

    [ANI-47-feedback-page] feat: 각 단계별 페이지 추가

--- apps/web/src/widgets/feedback/ui/MentoFeedback.tsx
+++ apps/web/src/widgets/feedback/ui/MentoFeedback.tsx
@@ -1,3 +1,24 @@
+import FeedbackList from './feedback-list'
+
 export function MentoFeedback() {
-  return <div>멘토 피드백</div>
+  return (
+    <div>
+      <FeedbackList>
+        <FeedbackList.ListTitle
+          courseName='HTML 개요(1)'
+          category='멘토'
+        />
+        <FeedbackList.QuestionTitle
+          questionIndex={1}
+          title='파트의 내용이 기대에 부합했나요?'
+        />
+        <FeedbackList.Likert />
+        <FeedbackList.QuestionTitle
+          questionIndex={2}
+          title='추가로 파트에 대해 남기고 싶은 의견이 있다면 자유롭게 기재해주세요.'
+        />
+        <FeedbackList.OpenEnded maxLength={500} />
+      </FeedbackList>
+    </div>
+  )
 }
