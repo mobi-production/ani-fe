99072438a0b58b5c9feeb49a1d9cf173c9cb9ee3
Zero1016 <iolpd42@gmail.com>
2024-11-21 15:39:17 +0900

    [ANI-33-Introduce] feat: path 상세조회 mock api를 추가합니다.

---
+++ apps/web/src/widgets/path/model/index.ts
@@ -0,0 +1,31 @@
+type PageFeature = 'LEARNING_CONTENTS' | 'QUIZ' | 'VIDEO'
+
+export type PathType = {
+  id: string | number
+  title: string
+  description: string
+  thumbnail: string
+  parts: PartType[]
+}
+
+export type PartType = {
+  id: string | number
+  title: string
+  order: string | number
+  page: PageType[]
+}
+
+export type PageType =
+  | {
+      id: string | number
+      title: string
+      order: string | number
+      type: Exclude<PageFeature, 'VIDEO'>
+    }
+  | {
+      id: string | number
+      title: string
+      order: string | number
+      type: 'VIDEO'
+      videoLength: string
+    }
