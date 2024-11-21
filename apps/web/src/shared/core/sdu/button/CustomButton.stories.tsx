b726f12284364a234fda2834c455c431ba6c1ca2
Zero1016 <iolpd42@gmail.com>
2024-11-21 15:31:15 +0900

    [ANI-33-Introduce] refactor: 버튼 스토리북 파일을 변경합니다.

---
+++ apps/web/src/shared/core/sdu/button/CustomButton.stories.tsx
@@ -0,0 +1,155 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ServerDrivenComponent, ServerDrivenComponentType } from '../ServerDrivenComponent'
+import { wrapWithAppRouterContext } from '@/shared/lib/utils/wrap-with-app-router-context'
+import { Flex } from '@repo/ui/server'
+import example from './example.json'
+
+const meta = {
+  title: 'SDU/Button',
+  component: ServerDrivenComponent,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: `
+#### 텍스트를 표시하는 컴포넌트
+
+#### 예시
+\`\`\`json
+{
+  "type": "Button",
+  "props": {
+    "text": "테스트",
+    "buttonType": "solid",
+    "style": {
+      "variant": "primary",
+      "size": "medium",
+      "fullWidth": false
+    },
+    "onClick": {
+      "type": "NAVIGATE",
+      "payload": {
+        "type": "PUSH",
+        "path": "/path"
+      }
+    }
+  }
+}
+\`\`\`
+
+
+#### 다음과 같은 함수를 사용할 수 있습니다.
+
+\`API_CALL\` : API 호출
+
+\`\`\`json
+{
+  "type": "API_CALL",
+  "endpoint": "/api/endpoint",
+  "method": "GET",
+  "payload": {}
+}
+\`\`\`
+
+\`NAVIGATE\` : 페이지 이동
+
+\`\`\`json
+{
+  "type": "NAVIGATE",
+  "payload": {
+    "type": "PUSH",
+    "path": "/path"
+  }
+}
+\`\`\`
+        `
+      }
+    }
+  },
+  decorators: [wrapWithAppRouterContext],
+  tags: ['autodocs']
+} satisfies Meta<typeof ServerDrivenComponent>
+
+export default meta
+
+export const Solid: StoryObj<typeof ServerDrivenComponent> = {
+  args: {
+    content: [
+      {
+        ...example,
+        props: {
+          ...example.props,
+          buttonType: 'solid'
+        }
+      } as ServerDrivenComponentType
+    ]
+  },
+  render: (args) => (
+    <Flex gap={12}>
+      <ServerDrivenComponent content={args.content} />
+      <ServerDrivenComponent
+        content={[
+          {
+            ...args.content[0],
+            props: { ...args.content[0]!.props, variant: 'secondary' }
+          } as ServerDrivenComponentType
+        ]}
+      />
+    </Flex>
+  )
+}
+
+export const Outlined: StoryObj<typeof ServerDrivenComponent> = {
+  args: {
+    content: [
+      {
+        ...example,
+        props: {
+          ...example.props,
+          buttonType: 'outlined'
+        }
+      } as ServerDrivenComponentType
+    ]
+  },
+  render: (args) => (
+    <Flex gap={12}>
+      <ServerDrivenComponent content={args.content} />
+      <ServerDrivenComponent
+        content={[
+          {
+            ...args.content[0],
+            props: { ...args.content[0]!.props, variant: 'secondary' }
+          } as ServerDrivenComponentType
+        ]}
+      />
+    </Flex>
+  )
+}
+
+export const Text: StoryObj<typeof ServerDrivenComponent> = {
+  args: {
+    content: [
+      {
+        ...example,
+        props: {
+          ...example.props,
+          buttonType: 'text'
+        }
+      } as ServerDrivenComponentType
+    ]
+  },
+  render: (args) => (
+    <Flex gap={12}>
+      <ServerDrivenComponent content={args.content} />
+      <ServerDrivenComponent
+        content={[
+          {
+            ...args.content[0],
+            props: { ...args.content[0]!.props, variant: 'secondary' }
+          } as ServerDrivenComponentType
+        ]}
+      />
+    </Flex>
+  )
+}
