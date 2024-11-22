9f3b78ad554f2912c45d589dffd69b1ce2dfd4ef
JeongwooHam <hamjw0122@gmail.com>
2024-11-23 02:35:05 +0900

    [ANI-32-floating-action-component] feat: 패스 페이지 layout에 floating component 추가

--- apps/web/src/shared/ui/floating-action/index.tsx
+++ apps/web/src/shared/ui/floating-action/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 function FloatingAction() {
   return (
-    <div className='flex h-[3.625rem] w-[3.625rem] cursor-pointer items-center justify-center rounded-full bg-label-normal hover:opacity-90 active:opacity-100'>
+    <div className='fixed bottom-[3.625rem] right-[3.625rem] z-10 flex h-[3.625rem] w-[3.625rem] cursor-pointer items-center justify-center rounded-full bg-label-normal hover:opacity-90 active:opacity-100'>
       <Image
         src='/webp/1on1chat.webp'
         width={32}
