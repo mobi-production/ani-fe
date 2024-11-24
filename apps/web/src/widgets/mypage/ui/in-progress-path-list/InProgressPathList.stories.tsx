4effa5495837b8805287d15bb61c56e30c46f8a0
Zero1016 <iolpd42@gmail.com>
2024-11-25 00:21:32 +0900

    [ANI-93-frontend-server-driven-ui] fix: 스토리북 에러를 수정합니다.

--- apps/web/src/widgets/mypage/ui/in-progress-path-list/InProgressPathList.stories.tsx
+++ apps/web/src/widgets/mypage/ui/in-progress-path-list/InProgressPathList.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react/*'
+import { Meta, StoryObj } from '@storybook/react'
 import InProgressPathList from '.'
 import { myPageData } from '@/__mock__/data/mypage'
 
