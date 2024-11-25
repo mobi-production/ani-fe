dab350bbe387cc05ec9bbce4bd396d9c390ca882
Zero1016 <iolpd42@gmail.com>
2024-11-25 20:14:50 +0900

    [ANI-93-frontend-server-driven-ui] refactor: callout의 설명을 개선합니다.

--- packages/sdu/src/components/callout/Callout.stories.tsx
+++ packages/sdu/src/components/callout/Callout.stories.tsx
@@ -2,7 +2,7 @@ import { ServerDrivenComponent, ServerDrivenComponentType } from '@repo/sdu'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import example from './example.json'
-
+import style from './style.json'
 const meta = {
   title: 'SDU/Callout',
   component: ServerDrivenComponent,
@@ -11,9 +11,17 @@ const meta = {
     docs: {
       description: {
         component: `
-#### Callout 컴포넌트
+도움말 또는 주의사항을 표시하는 컴포넌트입니다.
 
-#### 예시
+### props
+
+#### \`rich_text\`: 텍스트를 전달합니다.
+
+#### \`icon\`: 아이콘을 전달합니다.
+
+#### \`style\`: 컴포넌트의 색상과 배경색을 조절합니다.
+
+### example
 \`\`\`json
 ${JSON.stringify(example, null, 2)}
 \`\`\`
@@ -29,6 +37,27 @@ export default meta
 export const Preview: StoryObj<typeof ServerDrivenComponent> = {
   args: {
     content: [example as ServerDrivenComponentType]
+  },
+  render: (args) => (
+    <div className='w-[500px]'>
+      <ServerDrivenComponent content={args.content} />
+    </div>
+  )
+}
+
+export const WithStyle: StoryObj<typeof ServerDrivenComponent> = {
+  parameters: {
+    docs: {
+      description: {
+        story: `\`\`\`json
+${JSON.stringify(style, null, 2)}
+\`\`\`
+        `
+      }
+    }
+  },
+  args: {
+    content: [style as ServerDrivenComponentType]
   },
   render: (args) => (
     <div className='w-[500px]'>
