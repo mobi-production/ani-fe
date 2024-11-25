479996b04d51c0e3130eb8ed97c1038bcf8a6c83
zivivle <123868471+zivivle@users.noreply.github.com>
2024-11-26 00:00:15 +0900

    [ANI-34-auth-page] fix: storybook 파일 수정

--- apps/web/src/features/auth/ui/signup-modal/SignupModal.stories.tsx
+++ apps/web/src/features/auth/ui/signup-modal/SignupModal.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Flex } from '@repo/ui/server'
+import { Flex, Modal, SolidButton, Typography } from '@repo/ui/server'
 import SignupModal from '.'
+import Logo from '@/shared/ui/logo'
+import { Icon, Input } from '@repo/ui/client'
+import { useState } from 'react'
 
 const meta = {
   title: 'Feature/SignupModal',
@@ -19,16 +22,95 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Preview: Story = {
-  args: {},
-  decorators: [
-    (Story) => (
-      <Flex
-        align={'center'}
-        justify={'center'}
-        className='w-full'>
-        <Story />
-      </Flex>
+export const Primary: Story = {
+  render: () => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+    return (
+      <div className='flex h-screen items-center justify-center'>
+        <Modal
+          className='p-0'
+          withCloseButton={false}
+          withBackDrop
+          isOpen={true}
+          onClose={() => {}}>
+          <Flex
+            asChild
+            direction='column'
+            align='start'
+            className='min-w-[37.5rem] px-[3.125rem] py-[3rem]'>
+            <form className='gap-[3rem]'>
+              <Flex
+                justify='center'
+                className='w-full'>
+                <Logo />
+              </Flex>
+              <Flex
+                className='w-full'
+                direction='column'
+                gap={28}>
+                <Flex
+                  direction='column'
+                  gap={12}>
+                  <Typography
+                    variant='heading-2'
+                    fontWeight='semibold'>
+                    이메일
+                  </Typography>
+                  <Input placeholder='이메일을 입력해주세요' />
+                </Flex>
+              </Flex>
+              <Flex
+                className='w-full'
+                direction='column'
+                gap={28}>
+                <Flex
+                  direction='column'
+                  gap={12}>
+                  <Typography
+                    variant='heading-2'
+                    fontWeight='semibold'>
+                    닉네임
+                  </Typography>
+                  <Input placeholder='닉네임을 입력해주세요' />
+                </Flex>
+              </Flex>
+              <Flex
+                className='w-full'
+                direction='column'
+                gap={28}>
+                <Flex
+                  direction='column'
+                  gap={12}>
+                  <Typography
+                    variant='heading-2'
+                    fontWeight='semibold'>
+                    비밀번호
+                  </Typography>
+                  <Input
+                    placeholder='비밀번호를 입력해주세요'
+                    rightIcon={
+                      <div className='text-label-alternative'>
+                        <Icon
+                          size={20}
+                          onClick={() => setIsPasswordVisible((prev) => !prev)}
+                          name={isPasswordVisible ? 'EyeOutlined' : 'EyeInvisibleOutlined'}
+                        />
+                      </div>
+                    }
+                  />
+                </Flex>
+              </Flex>
+              <SolidButton
+                size='large'
+                fullWidth
+                type='submit'>
+                회원가입
+              </SolidButton>
+            </form>
+          </Flex>
+        </Modal>
+      </div>
     )
-  ]
+  }
 }
