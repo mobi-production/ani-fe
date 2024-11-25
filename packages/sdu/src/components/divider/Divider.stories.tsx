01c9ce8689ffc1bab0f66b50070655e3fae7ff70
Zero1016 <iolpd42@gmail.com>
2024-11-25 20:15:21 +0900

    [ANI-93-frontend-server-driven-ui] refactor: divider의 설명을 개선합니다.

--- packages/sdu/src/components/divider/Divider.stories.tsx
+++ packages/sdu/src/components/divider/Divider.stories.tsx
@@ -2,6 +2,7 @@ import { ServerDrivenComponent, ServerDrivenComponentType } from '@repo/sdu'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import example from './example.json'
+import style from './style.json'
 
 const meta = {
   title: 'SDU/Divider',
@@ -11,9 +12,13 @@ const meta = {
     docs: {
       description: {
         component: `
-#### Divider 컴포넌트
+구분선을 표시하는 컴포넌트입니다.
 
-#### 예시
+### props
+
+#### \`style\`: 컴포넌트의 배경색을 조절합니다.
+
+### example
 \`\`\`json
 ${JSON.stringify(example, null, 2)}
 \`\`\`
@@ -30,6 +35,32 @@ export default meta
 export const Default: StoryObj<typeof ServerDrivenComponent> = {
   args: {
     content: [example as ServerDrivenComponentType]
+  },
+  render: (args) => (
+    <div className='w-[300px] p-4'>
+      <ServerDrivenComponent content={args.content} />
+    </div>
+  )
+}
+
+export const WithStyle: StoryObj<typeof ServerDrivenComponent> = {
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        story: `
+컴포넌트의 선 색을 조절합니다.
+
+### example
+\`\`\`json
+${JSON.stringify(style, null, 2)}
+\`\`\`
+        `
+      }
+    }
+  },
+  args: {
+    content: [style as ServerDrivenComponentType]
   },
   render: (args) => (
     <div className='w-[300px] p-4'>
