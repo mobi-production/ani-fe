efb8b84d8b166839168b757308bfaf948a17887b
Zero1016 <iolpd42@gmail.com>
2024-11-25 20:14:30 +0900

    [ANI-93-frontend-server-driven-ui] refactor: rich_text를 통해 callout 텍스트 스타일을 변경합니다.

--- packages/sdu/src/components/callout/index.tsx
+++ packages/sdu/src/components/callout/index.tsx
@@ -1,30 +1,45 @@
+import cn from '@repo/sdu/libs/cn'
+import { ColorStyle } from '@repo/sdu/types/common'
 import { Flex, Typography } from '@repo/ui/server'
 
+import SDUText, { TextProps } from '../text'
+
 type Props = {
+  id?: string
   icon: string
-  text: string
+  rich_text: TextProps[]
+  style?: ColorStyle
 }
 
-function SDUCallout({ icon, text }: Props) {
+function SDUCallout({ icon, rich_text, style }: Props) {
   return (
     <Flex
       asChild
       gap={20}
       align='center'>
-      <div className='w-full rounded bg-cool-neutral-99 p-5'>
+      <div
+        className={cn(
+          'w-full rounded bg-cool-neutral-99 p-5',
+          style?.backgroundColor && 'bg-cool-neutral-99'
+        )}
+        style={style}>
         <Typography
           variant='heading-1'
           fontWeight='semibold'>
           {icon}
         </Typography>
-        <Typography
-          variant='body-1-normal'
-          fontWeight='medium'
-          className='whitespace-pre-wrap'>
-          {text}
-        </Typography>
+        <SDUCalloutText rich_text={rich_text} />
       </div>
     </Flex>
+  )
+}
+
+function SDUCalloutText({ rich_text }: { rich_text: TextProps[] }) {
+  return (
+    <SDUText
+      rich_text={rich_text}
+      tag='p'
+    />
   )
 }
 
