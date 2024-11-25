b477eadae1a68d9429a3718ebe87e9144817f29e
Zero1016 <iolpd42@gmail.com>
2024-11-25 20:17:55 +0900

    [ANI-93-frontend-server-driven-ui] docs: quote의 설명을 변경합니다.

--- packages/sdu/src/components/quote/Quote.stories.tsx
+++ packages/sdu/src/components/quote/Quote.stories.tsx
@@ -2,7 +2,7 @@ import { ServerDrivenComponent, ServerDrivenComponentType } from '@repo/sdu'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import example from './example.json'
-
+import style from './style.json'
 const meta = {
   title: 'SDU/Quote',
   component: ServerDrivenComponent,
@@ -11,7 +11,13 @@ const meta = {
     docs: {
       description: {
         component: `
-#### Quote 컴포넌트
+인용구를 표시하는 컴포넌트입니다.
+
+### props
+
+#### \`rich_text\`: 텍스트를 전달합니다.
+
+#### \`style\`: 컴포넌트의 색상과 배경색을 조절합니다.
 
 #### 예시
 \`\`\`json
@@ -35,4 +41,10 @@ export const Preview: StoryObj<typeof ServerDrivenComponent> = {
       <ServerDrivenComponent content={args.content} />
     </div>
   )
+}
+
+export const WithStyle: StoryObj<typeof ServerDrivenComponent> = {
+  args: {
+    content: [style as ServerDrivenComponentType]
+  }
 }
