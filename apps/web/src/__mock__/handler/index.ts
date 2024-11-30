9df98dc66ade450105cac5a7982dfdee13051bb2
zivivle <123868471+zivivle@users.noreply.github.com>
2024-11-30 09:07:50 +0900

    [ANI-34-auth-page] feat: mock 데이터 연동

--- apps/web/src/__mock__/handler/index.ts
+++ apps/web/src/__mock__/handler/index.ts
@@ -4,11 +4,13 @@ import { memberHandlers } from './member'
 import { pathHandlers } from './path'
 import { mainHandlers } from './main'
 import { myPageHandlers } from './mypage'
+import { authHandlers } from './auth'
 
 const handler: HttpHandler[] = [
   ...memberHandlers,
   ...pathHandlers,
   ...mainHandlers,
-  ...myPageHandlers
+  ...myPageHandlers,
+  ...authHandlers
 ]
 export default handler
