f0f6b390592015e88416b0e5b624dc8e7e84823c
Zero1016 <iolpd42@gmail.com>
2024-12-11 13:45:53 +0900

    [ANI-87-auth-api-connect] feat: env 설정을 추가합니다.

--- apps/web/src/shared/types/index.d.ts
+++ apps/web/src/shared/types/index.d.ts
@@ -2,4 +2,11 @@ import { ReactNode } from 'react'
 
 declare global {
   type PropsNeedChildren<P = unknown> = P & { children: ReactNode }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      ANI_WEB_AUTH_URL: string
+      ANI_WEB_API_URL: string
+    }
+  }
 }
